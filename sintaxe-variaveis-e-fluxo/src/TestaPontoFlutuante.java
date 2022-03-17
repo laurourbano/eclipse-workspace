7c84bc635819d1318d6f060c56356f8531b4e651
laurourbano <74082012+laurourbano@users.noreply.github.com>
2022-03-16 23:45:33 -0300

    Initial commit

---
+++ sintaxe-variaveis-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,15 @@
+
+public class TestaPontoFlutuante {
+	
+	public static void main(String[] args) {
+		
+		double salario;
+		salario = 1250.20;
+		System.out.println("O meu salário é " + salario);
+		
+		double divisao = 5.0 / 2.0;
+		System.out.println(divisao);
+		
+	}
+
+}
