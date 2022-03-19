fa78695354063ca763b69d166dcba716870181ad
laurourbano <74082012+laurourbano@users.noreply.github.com>
2022-03-19 17:48:20 -0300

    maisEstudosJava

---
+++ sintaxe-variaveis-e-fluxo/src/TestaValores.java
@@ -0,0 +1,11 @@
+
+public class TestaValores {
+
+	public static void main(String[] args) {
+		int primeiroNumero = 5;
+		int segundoNumero = 7;
+		segundoNumero = primeiroNumero;
+		System.out.println(segundoNumero);
+		
+	}
+}
