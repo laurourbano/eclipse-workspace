82fe3451eb8976c86983492d32058a69b8816b85
laurourbano <74082012+laurourbano@users.noreply.github.com>
2022-03-22 01:06:36 -0300

    2

---
+++ dio-java/src/Quadrilatero.java
@@ -0,0 +1,15 @@
+//sobrecarga
+public class Quadrilatero {
+	
+	public static void area(double lado) {
+		System.out.println("Area do quadrado " + lado * lado);
+	}
+
+	public static void area(double lado1, double lado2) {
+		System.out.println("Area do retangulo " + lado1 * lado2);
+	}
+
+	public static void area(double baseMaior, double baseMenor, double altura) {
+		System.out.println("Area do trapezio " + ((baseMenor + baseMaior) * altura) / 2);
+	}
+}
