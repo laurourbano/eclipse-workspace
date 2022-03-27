aa83a260e5ec56f6f2594e363c67146e83014c09
laurourbano <74082012+laurourbano@users.noreply.github.com>
2022-03-26 23:06:48 -0300

    1

--- bytebank-composto/src/TestaBanco.java
+++ bytebank-composto/src/TestaBanco.java
@@ -3,14 +3,14 @@ public class TestaBanco {
 
 	public static void main(String[] args) {
 		Cliente lauro = new Cliente();
-		lauro.nome = "Lauro Otávio Urbano";
-		lauro.cpf = "041.005.609-01";
-		lauro.profissao = "Desenvolvedor Java";
+		lauro.setNome("Lauro Otávio Urbano");
+		lauro.setCpf("041.005.609-01");
+		lauro.setProfissao("Desenvolvedor Java");
 		
 		Conta contaDoLauro = new Conta();
 		contaDoLauro.deposita(100);
 		
-		contaDoLauro.titular = lauro;
-		System.out.println(contaDoLauro.titular.nome);
+		//System.out.println(contaDoLauro.getTitular();
+		System.out.println(contaDoLauro.getSaldo());
 	}
 }
