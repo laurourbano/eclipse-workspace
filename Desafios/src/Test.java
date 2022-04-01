3476d060fd582ec02ea443b839833c3d95a33b5e
laurourbano <74082012+laurourbano@users.noreply.github.com>
2022-03-31 22:49:59 -0300

    123

---
+++ Desafios/src/Test.java
@@ -0,0 +1,13 @@
+	import java.util.Scanner;
+
+	public class Test {
+
+		public static void main(String[] args) {
+			int minutos;
+			Scanner input = new Scanner(System.in);
+			minutos = input.nextInt();
+			int K = 2 * minutos;
+			System.out.printf("%d minutos\n", K); // Digite aqui o calculo dos minutos
+		}
+	}
+
